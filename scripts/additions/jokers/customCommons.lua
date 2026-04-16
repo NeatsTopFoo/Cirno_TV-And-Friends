1a384f24c546bd485f62023e4e4e56ddece398dc
NeatsTopFoo <143466477+NeatsTopFoo@users.noreply.github.com>
2026-04-16 05:16:42 +0100

    please

--- scripts/additions/jokers/customCommons.lua
+++ scripts/additions/jokers/customCommons.lua
@@ -131,7 +131,7 @@ local jokerInfo = {
 			end,
 			
 			check_for_unlock = function(self, args)
-				return CirnoMod.config.jkrVals and CirnoMod.config.jkrVals[G.SETTINGS.profile] and CirnoMod.config.jkrVals[G.SETTINGS.profile].store CirnoMod.config.jkrVals[G.SETTINGS.profile].store.dabber_altf4
+				return CirnoMod.config.jkrVals and CirnoMod.config.jkrVals[G.SETTINGS.profile] and CirnoMod.config.jkrVals[G.SETTINGS.profile].store and CirnoMod.config.jkrVals[G.SETTINGS.profile].store.dabber_altf4
 			end,
 			
 			calculate = function(self, card, context)
